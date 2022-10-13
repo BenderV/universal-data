24a17abf1ef848fc7e3a405fabb2671249caf19b
Ben <benderville@gmail.com>
2022-10-13 11:45:30 +0200

    Rework scraper for dependencies

--- scraper.py
+++ scraper.py
@@ -4,20 +4,18 @@ from collections import defaultdict
 from datetime import datetime
 from parser import atom_parse
 
+import urllib3
+import yaml
 from requests import Session
 
-from move import transfert
+# from move import transfert
 from utils import PropertyTree, deep_get, dict_to_obj_tree, partial_format
-
-FETCHED = defaultdict(list)
-
-
-import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import urllib.parse
 
-logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("scaper")
+logger.setLevel(logging.DEBUG)
 
 
 class LiveServerSession(Session):
@@ -40,73 +38,127 @@ class LiveServerSession(Session):
 
 
 class Crawler:
-    def __init__(self, config, session):
+    def __init__(self, config):
+        self.items = defaultdict(list)  # FETCHED
         self.config = config
-        self.session = session
+        self.session = LiveServerSession(
+            config.host, headers=config.select("headers", {})
+        )
+        self.retrievers = [
+            type2class(entity.type)(self, entity) for entity in self.config.entities
+        ]
+        logger.debug(f"crawler host: {config.host}")
 
-    def run():
-        yield
+    def run(self):
+        # Take all routes from config and run them
+
+        for retriever in self.retrievers:
+            retriever.start()
+
+        print(self.items)
+
+    def _fetch(self, method, **attributes):
+        try:
+            logger.info(method, attributes)
+
+            # dumps = json.dumps(attributes)
+            # dumps2 = partial_format(attributes, **self.config.params.dict())
+            # attributes2 = json.loads(dumps2)
+            response = self.session.request(method, **attributes)
+            response.raise_for_status()
+        except Exception as err:
+            logger.error(err)
+            logger.error(err.response.text)
+            raise
+        return response
+
+    def add_item(self, entity, item):
+        self.items[entity].append(item)
+        for retriever in self.retrievers:
+            retriever.check_item(entity, item)
+
+
+# Create class retriever / route design / ... ?
+class Strategy:
+    def __init__(self, crawler, config):
+        self.crawler = crawler
+        self.config = config  # config could be just the name ?
+
+    def start(self):
+        raise NotImplementedError
+
+    def check_item(self, entity, item):
+        for dep in getattr(self.config, "dependencies", []):
+            if dep.entity == entity:
+                value = item[dep.entity_key]
+                key = dep.key
+                self.run({key: value})
+
+    def add_item(self, item):
+        self.crawler.add_item(self.config.entity, item)
 
     def _fetch(self, **attributes):
         method = getattr(
             self.config, "method", "GET"
         )  # TO HAVE IT EXPLICITELY IN CONFIG
-        try:
-            logging.debug(method, attributes)
-            response = self.session.request(method, **attributes)
-            response.raise_for_status()
-        except Exception as err:
-            logging.error(err)
-            logging.error(err.response.text)
-            raise
+        response = self.crawler._fetch(method, **attributes)
 
-        if self.config.format == "atom:1.0":
+        if self.config.format == "atom:1.0":  # should be moved ?
             return atom_parse(response.text)
         return response.json()
 
 
-class Looping(Crawler):
+class Looping(Strategy):
     max_value = None  # To Save and update ?
 
-    def run(self):
+    def start(self):
+        print("run looping")
         max_value = self._fetch_max_value()
-        logging.debug("max_value", max_value)  # TODO: to save
+        logger.debug(f"max_value: {max_value}")  # TODO: to save
         for ind, value in enumerate(range(max_value, 0, -1)):
             if ind > 2:
                 break  # Testing purpose
             url = self.config.request.url.format(value)
             result = self._fetch(url=url)
-            FETCHED[self.config.entity].append(result)
-            yield result
+            self.add_item(result)
 
     def _fetch_value(self, data, path):
         """TODO"""
         return data
 
     def _fetch_max_value(self):
-        res = self.session.get(self.config.max_value.url)
-        value = self._fetch_value(res.json(), self.config.max_value.key_path)
+        res = self._fetch(url=self.config.max_value.url)
+        value = self._fetch_value(res, self.config.max_value.key_path)
         return value
+
+
+class DirectFetch(Strategy):
+    def start(self):
+        pass
+
+    def run(self, params):
+        url = self.config.request.url.format(**params)
+        result = self._fetch(url=url)
+        self.add_item(result)
 
 
 class List(Crawler):
     """Listing without pagination"""
 
-    def run(self):
+    def start(self):
         response = self._fetch(url=self.config.request.url)
         results = deep_get(response, self.config.key)
         for result in results:
-            FETCHED[self.config.entity].append(result)
-        return response
+            self.add_item(result)
 
 
-class Listing(Crawler):
+class Listing(Strategy):
     """Listing with pagination"""
 
     def _fetch_list(self, cursor=None):
-        # if pagination
+
         request_attributes = getattr(self.config, "request", PropertyTree()).dict()
-        if hasattr(self.config, "pagination") and cursor is not None:
+        if hasattr(self.config, "pagination") and cursor is not None:  # if pagination
             type = self.config.pagination.type
             if isinstance(request_attributes[type], dict):
                 request_attributes[type][self.config.pagination.key] = cursor
@@ -117,13 +169,14 @@ class Listing(Crawler):
             else:
                 raise Exception('Request params should be "dict" or "str"')
 
+        print(request_attributes)
         response = self._fetch(**request_attributes)
         results = deep_get(response, self.config.key)
         for result in results:
-            FETCHED[self.config.entity].append(result)
+            self.add_item(result)
         return response
 
-    def run(self):
+    def start(self):
         cursor = getattr(self.config.pagination, "default", None)
 
         for ind in range(1, 3):  # Testing purpose
@@ -134,7 +187,6 @@ class Listing(Crawler):
                 cursor = (1 + ind) * self.config.pagination.step
             if not cursor:
                 break
-            yield response
 
 
 class Slicing(Listing):
@@ -146,7 +198,7 @@ class Slicing(Listing):
     def format_date(self, date):
         return date.strftime(self.config.slice.date_format)
 
-    def run(self):
+    def start(self):
         request_attributes = getattr(self.config, "request", PropertyTree()).dict()
         params = self.config.slice
         if isinstance(request_attributes[params.type], str):
@@ -159,20 +211,7 @@ class Slicing(Listing):
         else:
             raise Exception('Request params should be "dict" or "str"')
 
-        return super(Slicing, self).run()
-
-
-class DirectFetch(Crawler):
-    def run(self):
-        # Scan for task
-        for name, result in FETCHED.items():
-            if self.config.id.entity == name:
-                # HN sent null some times... ?
-                value = result[self.config.id.key]
-                url = self.config.request.url.format(value)
-                result = self._fetch(url=url)
-                FETCHED[self.config.entity].append(result)
-                yield result
+        return super(Slicing, self).start()
 
 
 def type2class(type):
@@ -190,26 +229,5 @@ def type2class(type):
 
 def runner(config):
     config = dict_to_obj_tree(config)
-    logging.debug(f"host: {config.host}")
-    session = LiveServerSession(config.host, headers=config.select("headers", {}))
-
-    crawlers = []
-    for entity in config.entities:
-        crawler = type2class(entity.type)(entity, session)
-        crawlers.append(crawler)
-
-    for crawler in crawlers:
-        logging.debug(f"Run Crawnler: {crawler.config.name}")
-        for result in crawler.run():
-            pass
-
-    logging.debug(FETCHED)
-    # for name, rows in FETCHED.items():
-    #    transfert(name, rows, "id")
-    with open("output.json", "w") as f:
-        json.dump(FETCHED, f)
-
-
-# Handle response
-# Parse
-# Next actions
+    crawler = Crawler(config)
+    crawler.run()
